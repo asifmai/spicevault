4c62e9d5aa878f6b243fff6596ccbf8d1891361d
iqbal <asifmai@hotmail.com>
2019-06-13 22:09:40 +0500

    'update'

--- pg/us.js
+++
@@ -1,6 +0,0 @@
-const _ = require('underscore');
-const a = [ "5cf2956b1771e6157cc2b256", "5cf296751771e6157cc2b257" ]
-const b = [ '5cf296751771e6157cc2b257', '5cf2956b1771e6157cc2b256' ]
-const c = _.intersection(a, b)
-console.log(a)
-// PlayerTwo.every(val => PlayerOne.includes(val));
